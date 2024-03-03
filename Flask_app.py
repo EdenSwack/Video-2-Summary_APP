c38fba8d3c122cc740ad579afde569d70483976a
EdenSwack <93931254+EdenSwack@users.noreply.github.com>
2024-03-03 22:28:28 +0200

    Update Flask_app.py

--- Flask_app.py
+++ Flask_app.py
@@ -3,44 +3,55 @@
 # python Flask_app.py
 
 from Video_Processor import Vid2Sum, translate
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS
+import requests
+
 
 app = Flask(__name__)
 CORS(app)
 app.debug = True
 
-# API call to function that gets all neccisary input from user, sends them to relevant function,
-# returns output and handels exception
 @app.route("/process_video", methods=["POST"])
 def process_video():
     if request.method == "POST":
-        video_file = request.files["video"]
-        filename = video_file.filename
-        filePath = "C:\\temp2\\" + filename
-        video_file.save(filePath)
+        try:
+            video_file = request.files["video"]
+            filename = video_file.filename
+            filePath = "C:\\temp2\\" + filename
+            video_file.save(filePath)
 
-        output_lang = request.form["outputLang"]
+            input_lang = request.form["inputLang"]
+            output_lang = request.form["outputLang"]
 
-        result = Vid2Sum(filePath)
+            result = Vid2Sum(filePath)
 
-        if output_lang.lower() != "english":
-            translated_text = translate(result, "en", output_lang)
-            if translated_text:
-                result = translated_text
-                return result
+            if output_lang.lower() != "english":
+                translated_text = translate(result, "en", output_lang)
+                if translated_text:
+                    result = translated_text
+                    return result
 
-        response = app.response_class(
-            response=result,
-            status=200,
-            headers={"Access-Control-Allow-Origin": "*"},
-        )
+            response = app.response_class(
+                response=result,
+                status=200,
+                headers={
+                    "Access-Control-Allow-Origin": "*"
+                },  
+            )
 
-        return response
+            return response
 
+        except KeyError as e:
+            # If 'text' key is not found in the response
+            return jsonify(error="Text not found in the response from audio-to-text model", original_error=str(e)), 500
+
+        except Exception as e:
+            # Catch any other exceptions and return an informative error message
+            return jsonify(error=str(e)), 500
     else:
         return "wrong method sent"
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=8080)
